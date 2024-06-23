cd76288227a04344464263cc8fc445897f3d3ab3
Muhammed Mukrim.M <117835488+Mukrim02@users.noreply.github.com>
2024-06-23 15:36:28 +0530

    Add files via upload
    Signed-off-by: Muhammed Mukrim.M <117835488+Mukrim02@users.noreply.github.com>

--- Pages/Product.jsx
+++ Pages/Product.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import {ShopContext} from '../Context/ShopContext';
+import { useParams } from "react-router-dom";
+import Breadcrums from "../Components/Breadcrums/Breadcrums";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProduct from "../Components/RlaatedProduct/RelatedProduct";
 
 const Product = () => {
-    return(
+    const{all_product}= useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e)=> e.id===Number(productId));
+    return (
         <div>
-        
+            <Breadcrums product={product}/>
+            <ProductDisplay product={product}/>
+            <DescriptionBox/>
+            <RelatedProduct/>
+
         </div>
     )
 
 }
-export default Product
+export default Product 
--- Pages/Shop.jsx
+++ Pages/Shop.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Hero from '../Components/Hero/Hero';
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewCollection from "../Components/New collection/NewCollection";
+import { NewsLetter } from "../Components/NewsLetter/NewsLetter";
 
 const Shop = () => {
-    return(
+    return (
         <div>
-        
+            <Hero />
+            <Popular />
+            <Offers/>
+            <NewCollection/>
+            <NewsLetter/>
         </div>
     )
 
